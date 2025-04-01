5476bd3addc2b58e4e1047522bde1bf503149b81
samil145 <samil.bayramli.sb@gmail.com>
2025-04-02 02:28:08 +0400

    Initial Commit

---
+++ RateMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RateMe
+//
+//  Created by Shamil Bayramli on 02.04.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
